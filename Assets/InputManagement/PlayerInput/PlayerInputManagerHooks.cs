d5fb003fecd2b326d4feffceeea40ad09264ac41
Brian Taylor Vann <brian@cashew.localdomain>
2020-08-02 20:31:53 -0700

    get multiplayer and input actions working

---
+++ Assets/InputManagement/PlayerInput/PlayerInputManagerHooks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Input.Plugins.PlayerInput;
+
+public class PlayerInputManagerHooks : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Instance should survive Scenes and Loading
+        DontDestroyOnLoad(this);
+    }
+
+    public void Ping(uint userId)
+    {
+        Debug.Log("User " + userId + "is read to play!");
+    }
+
+    void OnPlayerJoined(PlayerInput playerInput)
+    {
+
+    }
+
+    void OnPlayerLeft(PlayerInput playerInput)
+    {
+
+    }
+}
